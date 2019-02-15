7a17d48c624aeda44c6b516d7afbfc2adcabe6c9
EricT135 <eric.truong.135@gmail.com>
2019-02-14 23:43:35 -0700

    Add mouse move function

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@
 //*********************************************************************************** */
 // Navigation bar animation
 let barBxsoxClickOrder = 0;
-lset barBox = document.getElementById("barBox");
-=
-});
+let barBox = document.getElementById("barBox");
+
 barBox.axddEventListener("mouseenter",()=>{
     barBox.style.height="calc(70/160*100%)";
     if (barBoxClickOrder===0){
@@ -14,7 +13,7 @@ barBox.axddEventListener("mouseenter",()=>{
 });
 barBox.addEventListener("mouseleave",()=>{
     barBox.style.height="calc(50/160*100%)";
-    if 
+    if(!barBox) {
         barIconContainer.style.backgroundColor="rgba(255,255,255,0.7)";
     }
 });
@@ -25,19 +24,9 @@ barBox.addEventListener("mouseleave",()=>{
 window.onscroll = function(){
     var homeDisplay = document.getElementById("homeDisplay");
     var videoHeight = homeDisplay.clientHeight*0.3;
-    if (startingVideo != 0){
-        if (window.pageYOffset >= videoHeight) {
-            document.getElementById("homeVideo").pause();
-        } else{
-            document.getElementById("homeVideo").play();
-        }           
 
-    }
     if ( window.pageYOffset > homeDisplay.clientHeight*0.6 && window.pageYOffset < homeDisplay.clientHeight) {
-        window.scrollTo({
-            top: homeDisplay.clientHeight,
-            behavior: 'smooth'
-        });
+        window.pageYOffset.clientX =190px;
     }
 };
 // ****************************************************************************************
